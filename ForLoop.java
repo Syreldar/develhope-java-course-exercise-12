4e4ecc07fb0a72993cd39548c237e17807062f6c
Syreldar <enrico.drago3@gmail.com>
2023-07-11 14:52:14 +0200

    Upload exercise

--- ForLoop.java
+++ ForLoop.java
@@ -1,6 +1,13 @@
 public class ForLoop
 {
+    private static final int START_VALUE = 5;
+    private static final int END_VALUE = 25;
+
     public static void main(String[] args)
     {
+        for (int i = START_VALUE; i <= END_VALUE; i++)
+        {
+            System.out.println(i);
+        }
     }
 }
